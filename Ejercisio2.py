52ec326d9d4fa30284507fb08742b928672e3306
Unicomfacauca <unicomfacauca@PPN-MAC-PC06.local>
2025-03-08 15:05:10 -0500

    Solucion del ejercisio dos

---
+++ Ejercisio2.py
@@ -0,0 +1,11 @@
+## Ejercicio 2:## Ejercicio 2:
+#Dada una frase averiguar cuantas vocales tiene y mostrarlas. Para esto, se debe usar la siguiente lista ```vowels = ['a', 'e', 'i', 'o', 'u']```
+##externocleidomastoideo 11
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+frase = 'unicomfacauca'
+frb = list(frase)
+
+
+print(frase,frb.count(vowels[0])+frb.count(vowels[1])+frb.count(vowels[2])+frb.count(vowels[3])+frb.count(vowels[4]))
+
